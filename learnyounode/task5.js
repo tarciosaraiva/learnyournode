b2d98d0100b1f6b219b7a509fe730caee99dbd19
Tarcio <tarcio@gmail.com>
2014-04-14 18:40:08 +1000

    Task 5: Filtering files in a directory

---
+++ learnyounode/task5.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+String.prototype.endsWith = function (suffix) {
+  var l = this.length - suffix.length;
+  return l >= 0 && this.indexOf(suffix, l) === l;
+};
+
+if (process.argv.length !== 4) {
+  console.error('Format is node task4.js <dir> <extension>');
+  process.exit(0);
+}
+
+var dir = process.argv[2];
+var ext = '.' + process.argv[3];
+
+fs.readdir(dir, function (err, files) {
+
+  if (err) {
+    console.log('Could not read directory: ' + err);
+    process.exit(0);
+  }
+
+  files
+    .filter(function (el) {
+      return el.endsWith(ext);
+    })
+    .forEach(function (el) {
+      console.log(el);
+    });
+
+});
