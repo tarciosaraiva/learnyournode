08edc1ec534bbf12ce737d2522adf5e1c44b5bb6
Tarcio <tarcio@gmail.com>
2014-04-23 14:22:47 +1000

    Task 5: Buffer Concat (solution 2)

--- bytewiser/task5.js
+++ bytewiser/task5.js
@@ -1,9 +1,25 @@
 var buffs = [];
 
+// *************
 // solution capturing all data
+// *************
+// process.stdin
+//   .on('data', function (data) {
+//     buffs.push(data);
+//   })
+//   .on('end', function () {
+//     console.log(Buffer.concat(buffs));
+//   });
+
+// *************
+// solution capturing chunks of data
+// *************
 process.stdin
-  .on('data', function (chunk) {
-    buffs.push(chunk);
+  .on('readable', function () {
+    var chunk;
+    while (null !== (chunk = process.stdin.read())) {
+      buffs.push(chunk);
+    }
   })
   .on('end', function () {
     console.log(Buffer.concat(buffs));
